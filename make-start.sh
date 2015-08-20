4e468fd8dc75368fad2f4e8febe8149d823515c0
Andrew Schwartz <andrew.schwartz@gengo.com>
2015-08-20 18:29:26 +0900

    add procfile

--- make-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "============================================"
-echo " Still missing her... "
-echo "============================================"
-
-godep go run main.go
-
-exit 0
