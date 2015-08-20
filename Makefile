e9e619d0423ed64d3f68a11449fcaa1d094cd1da
Winston <winstonhowes@gmail.com>
2015-08-20 00:16:10 -0700

    added make start because we fancy

---
+++ Makefile
@@ -0,0 +1,2 @@
+start:
+	./make-start.sh
---
+++ make-start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# load common library functions
+[ -r common.sh ] && source common.sh
+
+echo "============================================"
+echo " Still missing her... "
+echo "============================================"
+
+go run main.go
+
+exit 0
